c56f565e407b60202c71d362ee018d4fae3d2585
Minxuan Lin <James.lin1314520@outlook.com>
2020-04-28 14:11:40 +0930

    Added functionality for opened demonstrations to close, fixed problem where demonstrations stay open when exiting and re-entering step/method 1 if any is open
    Signed-off-by: Minxuan Lin <James.lin1314520@outlook.com>

--- js/js.js
+++ js/js.js
@@ -39,6 +39,10 @@ function show__start() {
   document.getElementById("techniques_item1").style.display = "none";
   document.getElementById("materials__recipe1").style.display = "none";
   document.getElementById("start__recipe1").style.display = "block";
+  document.getElementById('vid_window').style.display = "none";
+  document.getElementById('gif_window').style.display = "none";
+  document.getElementById('pic_window').style.display = "none";
+  document.getElementById('close_all_button').style.display = "none";
 }
 
 
@@ -53,17 +57,26 @@ function show_techiques_item1() {
 
 
 function pic_layer(){
+    document.getElementById('close_all_button').style.display = "block";
     document.getElementById('pic_window').style.display = "block";
     document.getElementById('gif_window').style.display = "none";
     document.getElementById('vid_window').style.display = "none";
   }
 function gif_layer(){
+    document.getElementById('close_all_button').style.display = "block";
     document.getElementById('gif_window').style.display = "block";
     document.getElementById('pic_window').style.display = "none";
     document.getElementById('vid_window').style.display = "none";
         }
 function vid_layer(){
+    document.getElementById('close_all_button').style.display = "block";
     document.getElementById('vid_window').style.display = "block";
+    document.getElementById('gif_window').style.display = "none";
+    document.getElementById('pic_window').style.display = "none";
+  }
+function close_all(){
+    document.getElementById('close_all_button').style.display = "none";
+    document.getElementById('vid_window').style.display = "none";
     document.getElementById('gif_window').style.display = "none";
     document.getElementById('pic_window').style.display = "none";
   }
